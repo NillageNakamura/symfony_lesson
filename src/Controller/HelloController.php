1bd5dd5cb5610ca7648ce6334f98018224d7758b
NillageNakamura <nillage.tharma@gmail.com>
2021-12-25 14:45:41 +0900

    route修正

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
-    #[Route('/hello', name: 'hello')]
-    public function index()
+    #[Route('/hello/{name}/{pass}', name: 'hello')]
+    public function index($name, $pass)
     {
-        return new Response('hello symfony!!');
+        $result = '<html><body>';
+        $result .= '<h1>Parameter</h1>';
+        $result .= '<p>name: ' . $name . '</p>';
+        $result .= '<p>pass: ' . $pass . '</p>';
+        $result .= '</body></html>';
+        return new Response($result);
     }
 }
