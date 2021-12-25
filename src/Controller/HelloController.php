b9d22e1feb3a8cb8c51b554da253ae4c85dc962a
NillageNakamura <nillage.tharma@gmail.com>
2021-12-25 18:27:44 +0900

    テンプレートにフォームを表示

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,35 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello', name: 'hello')]
     public function index(Request $request)
     {
+        $form = $this->createFormBuilder()
+            ->add('input', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
         if ($request->getMethod() == 'POST'){
-            $input = $request->request->get('input');
-            $msg = 'こんにちわ' . $input . 'さん';
+            $form->handleRequest($request);
+            $msg = 'こんにちは' . $form->get('input')->getData() . 'さん';
         } else {
             $msg = 'お名前は？';
         }
         return $this->render('hello/index.html.twig', [
             'title' => 'Hello',
             'message' => $msg,
-        ]);
-    }
-
-    #[Route('/other', name: 'other')]
-    public function other(Request $request)
-    {
-        $input = $request->request->get('input');
-        $msg = 'こんにちわ' . $input . 'さん';
-        return $this->render('hello/index.html.twig', [
-            'title' => 'Hello!!!',
-            'message' => $msg,
+            'form' => $form->createView(),
         ]);
     }
 }
