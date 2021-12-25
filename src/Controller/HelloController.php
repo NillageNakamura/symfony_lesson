5133547fcfa9a82559ad5f8f48dc703024f6ccc0
NillageNakamura <nillage.tharma@gmail.com>
2021-12-25 20:37:41 +0900

    セッションの利用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,27 +8,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello', name: 'hello')]
-    public function index(Request $request)
+    public function index(Request $request, SessionInterface $session)
     {
-        $form = $this->createFormBuilder()
-            ->add('input', TextType::class)
+        $data = new MyData();
+        $form = $this->createFormBuilder($data)
+            ->add('data', TextType::class)
             ->add('save', SubmitType::class, ['label' => 'Click'])
             ->getForm();
 
         if ($request->getMethod() == 'POST'){
             $form->handleRequest($request);
-            $msg = 'こんにちは' . $form->get('input')->getData() . 'さん';
-        } else {
-            $msg = 'お名前は？';
+            $data = $form->getData();
+            if ($data->getData() == '!'){
+                $session->remove('data');
+            } else {
+                $session->set('data', $data->getData());
+            }
         }
+
         return $this->render('hello/index.html.twig', [
             'title' => 'Hello',
-            'message' => $msg,
+            'data' => $session->get('data'),
             'form' => $form->createView(),
         ]);
+    }
+}
+class Mydata
+{
+    protected $data = '';
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
     }
 }
