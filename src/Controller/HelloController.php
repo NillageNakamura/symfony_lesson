653dcb68c61ddc53047a569dfb283c588e688318
NillageNakamura <nillage.tharma@gmail.com>
2021-12-26 13:41:25 +0900

    レコードの更新

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class HelloController extends AbstractController
 {
@@ -32,19 +33,61 @@ class HelloController extends AbstractController
             'data' => $person,
         ]);
     }
-}
 
-class FindForm
-{
-    private $find;
-
-    public function getFind()
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
     {
-        return $this->find;
+        $person = new Person();
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        if ($request->getMethod() == 'POST'){
+            // リクエスト情報をフォームにハンドリング
+            $form->handleRequest($request);
+            // createFormBuilderに引数を指定しておくとgetDataでインスタンスを取り出せる
+            $person = $form->getData();
+            // マネージャを取得し、persistを使ってインスタンスを保存
+            $manager = $doctrine->getManager();
+            $manager->persist($person);
+            // flushで反映
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig', [
+                'title' => 'Hello',
+                'message' => 'Create Entity',
+                'form' => $form->createView(),
+            ]);
+        }
     }
 
-    public function setFind($find)
+    #[Route('/update/{id}', name: 'update')]
+    public function update(Request $request, ManagerRegistry $doctrine, Person $person, $id=1)
     {
-        $this->find = $find;
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $doctrine->getManager();
+            $manager->persist($person);
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig', [
+                'title' => 'Hello',
+                'message' => 'Update Entity id=' . $person->getId(),
+                'form' => $form->createView(),
+            ]);
+        }
     }
 }
