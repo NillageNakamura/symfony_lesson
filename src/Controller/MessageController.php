5c7076163d0428eba8e17908778c5019a5e07a7a
NillageNakamura <nillage.tharma@gmail.com>
2021-12-26 22:53:32 +0900

    リレーションの作成

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Message;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MessageController extends AbstractController
+{
+    #[Route('/message', name: 'message')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getManager()->getRepository(Message::class);
+        $data = $repository->findAll();
+        return $this->render('message/index.html.twig', [
+            'title' => 'Message',
+            'data' => $data,
+        ]);
+    }
+
+    #[Route('message/create', name: 'create')]
+    public function create(Request $request, ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    {
+        $message = new Message();
+        $form = $this->createForm(messageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST'){
+            $message = $form->getData();
+            $errors = $validator->validate($message);
+
+            if (count($errors) == 0) {
+                $manager = $doctrine->getManager();
+                $manager->persist($message);
+                $manager->flush();
+                return $this->redirect('/message');
+            } else {
+                $msg = "oh...can't posted...";
+            }
+        } else {
+            $msg = 'type your message!';
+        }
+        return $this->render('message/create.html.twig', [
+            'title' => 'Hello',
+            'message' => $msg,
+            'form' => $form->createView(),
+        ]);
+    }
+}
