fe030821f68daa69d7aef347779d8157c2704505
NillageNakamura <nillage.tharma@gmail.com>
2021-12-26 12:24:55 +0900

    自動フィッチ

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,6 +7,8 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
@@ -20,5 +22,29 @@ class HelloController extends AbstractController
             'title' => 'Hello',
             'data' => $data,
         ]);
+    }
+
+    #[Route('/find/{id}', name: 'find')]
+    public function find(Request $request, Person $person): Response
+    {
+        return $this->render('hello/find.html.twig', [
+            'title' => 'Hello',
+            'data' => $person,
+        ]);
+    }
+}
+
+class FindForm
+{
+    private $find;
+
+    public function getFind()
+    {
+        return $this->find;
+    }
+
+    public function setFind($find)
+    {
+        $this->find = $find;
     }
 }
